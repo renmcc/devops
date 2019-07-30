8683241b630d172283b5a919aeecc2515faef745
ren_mcc <ren_mcc@foxmail.com>
2019-07-30 14:49:20 +0800

    modified apps

--- apps/idcs/urls.py
+++ apps/idcs/urls.py
@@ -3,8 +3,11 @@
 #__time__:2019/7/250:09
 #__author__:"ren_mcc"
 from django.conf.urls import url
-from views import idc_list
+from views import idc_list,idc_list_v2,idc_detail_v2
 
 urlpatterns = [
-    url("^idcs/$", idc_list)
+    url("^idcs/$", idc_list),
+    url("^idcs/$", idc_list),
+    url("^idcs_v2/$", idc_list_v2),
+    url("^idcs_detail_v2/(?P<pk>[0-9]+)/$", idc_detail_v2)
 ]
--- apps/idcs/views.py
+++ apps/idcs/views.py
@@ -9,6 +9,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
 # Create your views here.
+##################################版本一###########################################
 class JSONResponse(HttpResponse):
     def __init__(self,data,**kwargs):
         kwargs.setdefault('content_type','application/json')
@@ -29,3 +30,58 @@ def idc_list(request, *args, **kwargs):
             serializer.save()
             return JSONResponse(serializer.data)
     return HttpResponse("")
+
+
+##################################版本二###########################################
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+
+@api_view(["GET","POST"])
+def idc_list_v2(request,*args,**kwargs):
+    if request.method == "GET":
+        queryset = Idc.objects.all()
+        serializer = IdcSerializer(queryset, many=True)
+        return JSONResponse(serializer.data)
+    elif request.method == "POST":
+        serializer = IdcSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JSONResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET","PUT","DELETE"])
+def idc_detail_v2(request,pk,*args,**kwargs):
+    try:
+        idc = Idc.objects.get(pk=pk)
+    except Idc.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = IdcSerializer(idc)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = IdcSerializer(idc, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == "DELETE":
+        idc.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
