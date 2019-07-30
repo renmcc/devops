205b974e47070fbb2f09e0c2305ee19aeac87460
ren_mcc <ren_mcc@foxmail.com>
2019-07-30 09:44:44 +0800

    init

--- devops/apps/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-#coding:utf8
-#__time__:2019/7/250:01
-#__author__:"ren_mcc"
--- devops/apps/idcs/urls.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#coding:utf8
-#__time__:2019/7/250:09
-#__author__:"ren_mcc"
-urlpatterns = [
-]
