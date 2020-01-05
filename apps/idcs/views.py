e5fb547c374c2ed7e4a80469831ed0bbd67e5d52
ren_mcc <ren_mcc@foxmail.com>
2020-01-05 13:09:21 +0800

    idc_seralizer

--- apps/idcs/views.py
+++ apps/idcs/views.py
@@ -3,9 +3,57 @@ from __future__ import unicode_literals
 from idcs.models import  Idc
 from .serializers import IdcSerializer
 from rest_framework import viewsets
-
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
+
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        kwargs.setdefault('content_type', "application/json")
+        content = JSONRenderer().render(data)
+        super(JSONResponse, self).__init__(content=content, **kwargs)
+
+def idc_list(request, *args, **kwargs):
+    if request.method == 'GET':
+        queryset = Idc.objects.all()
+        serializer = IdcSerializer(queryset, many=True)
+        return JSONResponse(serializer.data)
+        # content = JSONRenderer().render(serializer.data)
+        # return HttpResponse(content, content_type="application/json")
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = IdcSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+            # content = JSONRenderer().render(serializer.data)
+            # return HttpResponse(content, content_type="application/json")
+    return HttpResponse("")
+
+def idc_detail(request, pk, *args, **kwargs):
+    try:
+        idc = Idc.objects.get(pk=pk)
+    except Idc.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = IdcSerializer(idc)
+        return JSONResponse(serializer.data)
+
+    elif request.method == "PUT":
+        content = JSONParser().PARSE(request)
+        serializer = IdcSerializer(idc, data = content)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        idc.delete()
+        return HttpResponse(status=204)
+
 class IdcViewset(viewsets.ModelViewSet):
     """
     retrieve:
--- devops/urls.py
+++ devops/urls.py
@@ -14,33 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
-from django.contrib import admin
-from rest_framework.routers import DefaultRouter
-from users.views import UserViewset,DashboardStatusViewset
-from rest_framework.documentation import include_docs_urls
-from idcs.views import IdcViewset
-from cabinet.views import CabinetViewset
-from manufacturer.views import ManufacturerViewset,ProductModelViewset
-from servers.views import  ServerAutoReportViewset,NetworkDeviceViewset,IPViewset,ServerViewset
-
-route = DefaultRouter()
-route.register("idcs", IdcViewset, base_name="idcs")
-route.register("users", UserViewset, base_name="users")
-route.register("cabinet", CabinetViewset, base_name="cabinet")
-route.register("Manufacturer", ManufacturerViewset, base_name="Manufacturer")
-route.register("ProductModel", ProductModelViewset, base_name="ProductModel")
-route.register("ServerAutoReport", ServerAutoReportViewset, base_name="ServerAutoReport")
-route.register("NetworkDevice", NetworkDeviceViewset, base_name="NetworkDevice")
-route.register("IP", IPViewset, base_name="IP")
-route.register("Server", ServerViewset, base_name="Server")
-route.register("Dashboard", DashboardStatusViewset, base_name="Dashboard")
-
-urlpatterns = [
-    url(r'^', include(route.urls)),
-    url(r'^api-auth', include("rest_framework.urls",namespace="rest_framework")),
-    url(r'^docs/', include_docs_urls("运维平台接口文档"))
-]
+# from django.conf.urls import url,include
+# from django.contrib import admin
+# from rest_framework.routers import DefaultRouter
+# from users.views import UserViewset,DashboardStatusViewset
+# from rest_framework.documentation import include_docs_urls
+# from idcs.views import IdcViewset
+# from cabinet.views import CabinetViewset
+# from manufacturer.views import ManufacturerViewset,ProductModelViewset
+# from servers.views import  ServerAutoReportViewset,NetworkDeviceViewset,IPViewset,ServerViewset
+#
+# route = DefaultRouter()
+# route.register("idcs", IdcViewset, base_name="idcs")
+# route.register("users", UserViewset, base_name="users")
+# route.register("cabinet", CabinetViewset, base_name="cabinet")
+# route.register("Manufacturer", ManufacturerViewset, base_name="Manufacturer")
+# route.register("ProductModel", ProductModelViewset, base_name="ProductModel")
+# route.register("ServerAutoReport", ServerAutoReportViewset, base_name="ServerAutoReport")
+# route.register("NetworkDevice", NetworkDeviceViewset, base_name="NetworkDevice")
+# route.register("IP", IPViewset, base_name="IP")
+# route.register("Server", ServerViewset, base_name="Server")
+# route.register("Dashboard", DashboardStatusViewset, base_name="Dashboard")
+#
+# urlpatterns = [
+#     url(r'^', include(route.urls)),
+#     url(r'^api-auth', include("rest_framework.urls",namespace="rest_framework")),
+#     url(r'^docs/', include_docs_urls("运维平台接口文档"))
+# ]
 
 
 # urlpatterns = [
@@ -51,3 +51,11 @@ urlpatterns = [
 #     # url(r'^cmdb/', include("cmdb.urls"))
 # ]
 
+
+from django.conf.urls import url,include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^idc/', include("idcs.urls")),
+]
