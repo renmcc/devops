b6204313dc4aa488b0cb34b71daa0eaf011a9024
ren_mcc <ren_mcc@foxmail.com>
2020-02-04 18:07:26 +0800

    删除idc的status字段

--- apps/idcs/models.py
+++ apps/idcs/models.py
@@ -10,7 +10,7 @@ class Idc(models.Model):
     phone = models.CharField("联系人",max_length=15)
     email = models.EmailField("邮件地址",default="null")
     letter = models.CharField("IDC简称",max_length=5)
-    status = models.BooleanField("状态",default=True)
+    #status = models.BooleanField("状态",default=True)
 
     def __str__(self):
         return self.name
--- devops/settings.py
+++ devops/settings.py
@@ -257,13 +257,13 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    #使用django的模型权限
-    'DEFAULT_PERMISSION_CLASSES': (
-        #'rest_framework.permissions.DjangoModelPermissions',
-        #必须得登录
-        'rest_framework.permissions.IsAuthenticated',
-        #使用自定义权限
-        'devops.permissions.Permissions',
-    ),
+    # #使用django的模型权限
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     #'rest_framework.permissions.DjangoModelPermissions',
+    #     #必须得登录
+    #     'rest_framework.permissions.IsAuthenticated',
+    #     #使用自定义权限
+    #     'devops.permissions.Permissions',
+    # ),
 }
 SILENCED_SYSTEM_CHECKS = ['mysql.E001']
